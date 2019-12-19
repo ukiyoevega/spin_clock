1d18a3045be91d8ace069e26404993e7971374a9
vv <610537467@qq.com>
2019-12-19 17:11:48 +0800

    align clock faces to current date time

--- spin_clock/lib/clock_face_painter.dart
+++ spin_clock/lib/clock_face_painter.dart
@@ -3,6 +3,7 @@ import 'dart:math';
 
 class ClockFacePainter extends CustomPainter {
   final double trackerPosition;
+  final DateTime dateTime;
   final Paint trackerPaint = new Paint()
           ..color = Colors.black
           ..style = PaintingStyle.fill;
@@ -11,10 +12,10 @@ class ClockFacePainter extends CustomPainter {
   double borderWidth;
   final Paint dialPaint = new Paint();
   final TextPainter textPainter = TextPainter(textAlign: TextAlign.center,
-          textDirection: TextDirection.rtl,
+          textDirection: TextDirection.ltr,
         );
 
-  ClockFacePainter({this.trackerPosition});
+  ClockFacePainter({this.dateTime, this.trackerPosition});
 
   @override
   void paint(Canvas canvas, Size size) {
@@ -37,14 +38,52 @@ class ClockFacePainter extends CustomPainter {
   bool shouldRepaint(ClockFacePainter oldDelegate) {
     return true;
   }
+  
+  final hourTextStyle = TextStyle(fontFamily: 'Poppins', fontWeight: FontWeight.w200, fontSize:13.0);
+  final minuteTextStyle = TextStyle(fontFamily: 'Poppins', fontWeight: FontWeight.w200, fontSize:10.0);
+  final largeTextStyle = TextStyle(color: Color(0xFF333333), fontFamily: 'Poppins', fontWeight: FontWeight.w400, fontSize:100.0);
+
+  int _hourText({int i, int digitOffset}) { // text offset by digitOffset on the clock
+    final hour = (dateTime.hour > 12) ? (dateTime.hour - 12) : dateTime.hour;
+    int hourOffset = hour - 2;
+    int text = (i+digitOffset)~/5;
+    if (text + hourOffset > 12) {
+      return text + hourOffset - 12;
+    } else {
+      return text + hourOffset;
+    }
+  }
+
+  int _minuteText({int i}) {
+    int minuteOffset = dateTime.minute - 38;
+    if (minuteOffset + i < 0) {
+      return minuteOffset + i + 60;
+    } else if (minuteOffset + i >= 60) {
+      return minuteOffset + i - 60;
+    } else {
+      return minuteOffset + i;
+    }
+  }
 
   void _drawDigit({bool hourMode = true, Canvas canvas, int i, int digitOffset}) {
     canvas.save();
     canvas.translate(0.0, -radius+borderWidth+14);
-    textPainter.text= new TextSpan(
-      text: hourMode ? '${i+digitOffset == 0 ? 12 : (i+digitOffset)~/5}' : '$i',
-      style: TextStyle(fontFamily: 'Poppins', fontSize: hourMode ? 20.0 : 10),
-    );
+    if (hourMode) {
+      int hourText = _hourText(i: i, digitOffset: digitOffset);
+      int currentHour = (dateTime.hour > 12) ? (dateTime.hour - 12) : dateTime.hour;
+      bool isCurrent = hourText == currentHour;
+      if (isCurrent) { canvas.translate(0, 40); }
+      textPainter.text= new TextSpan(text: '$hourText',
+        style: isCurrent ? largeTextStyle : hourTextStyle);
+    } else {
+      int minuteText = _minuteText(i: i);
+      bool isCurrent = minuteText == dateTime.minute;
+      if (isCurrent) { canvas.translate(-10, 70); }
+      textPainter.text= new TextSpan(
+        text: '${minuteText.toString().padLeft(2, '0')}', 
+        children: [], // isCurrent ? [TextSpan(text: " PM", style: largeTextStyle.copyWith(fontSize: 30))]: 
+        style: isCurrent ? largeTextStyle : minuteTextStyle);
+    }
     canvas.rotate(-angle*i);
     textPainter.layout();
     var painterOffset = new Offset(-(textPainter.width/2), -(textPainter.height/2));

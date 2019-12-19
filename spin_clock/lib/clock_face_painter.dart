c453e38991bfc40b403aff8e1ea1d800d8cd27d6
vv <610537467@qq.com>
2019-12-19 14:16:38 +0800

    add clock face painter for 2 half-size faces

---
+++ spin_clock/lib/clock_face_painter.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+class ClockFacePainter extends CustomPainter {
+  double radius;
+  double angle;
+  double borderWidth;
+  final Paint dialPaint = new Paint();
+  final TextPainter textPainter = TextPainter(textAlign: TextAlign.center,
+          textDirection: TextDirection.rtl,
+        );
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    borderWidth = size.width*0.02; 
+    angle = 2 * pi / 60;
+    radius = size.width/2;
+    canvas.save();
+
+    canvas.translate(size.width*0.05, size.height);
+    _drawClockFace(canvas: canvas, digitOffset: 3);
+
+    canvas.translate(size.width*0.9, -size.height);
+    _drawClockFace(hourMode: false, canvas: canvas, digitOffset: 0);
+
+    canvas.restore();
+  }
+
+  @override
+  bool shouldRepaint(ClockFacePainter oldDelegate) {
+    return true;
+  }
+
+
+  void _drawDigit({bool hourMode = true, Canvas canvas, int i, int digitOffset}) {
+    canvas.save();
+    canvas.translate(0.0, -radius+borderWidth+14);
+    textPainter.text= new TextSpan(
+      text: hourMode ? '${i+digitOffset == 0 ? 12 : (i+digitOffset)~/5}' : '$i',
+      style: TextStyle(fontFamily: 'Poppins', fontSize: hourMode ? 20.0 : 10),
+    );
+    canvas.rotate(-angle*i);
+    textPainter.layout();
+    var painterOffset = new Offset(-(textPainter.width/2), -(textPainter.height/2));
+    textPainter.paint(canvas, painterOffset);
+    canvas.restore();
+  }
+
+  void _drawClockFace({bool hourMode = true, Canvas canvas, int digitOffset}) {
+    dialPaint.color = Color(0xFFFAFAFA);
+    canvas.drawCircle(Offset(0.0, 0.0), radius, dialPaint);
+    dialPaint.color = Colors.white;
+    canvas.drawCircle(Offset(0.0, 0.0), radius-borderWidth, dialPaint);
+    dialPaint.color = Color(0xFF999999);
+    for (var i = 0; i < 60; i++ ) {
+      dialPaint.strokeWidth = 0.5;
+      canvas.drawLine(
+          new Offset(0.0, -radius), new Offset(0.0, -radius+borderWidth), dialPaint);
+      if (hourMode) {
+        if ((i+digitOffset)%5==0){
+        _drawDigit(canvas: canvas, i: i, digitOffset: digitOffset);
+       }
+      } else {
+        _drawDigit(hourMode: false, canvas: canvas, i: i, digitOffset: digitOffset);
+      }
+      canvas.rotate(angle);
+    }
+  }
+}

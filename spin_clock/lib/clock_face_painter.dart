81c13ca424d1a23fde9a1b97bdd43c4944f210c5
vv <610537467@qq.com>
2019-12-20 16:20:54 +0800

    enlarge current time text size

--- spin_clock/lib/clock_face_painter.dart
+++ spin_clock/lib/clock_face_painter.dart
@@ -41,7 +41,7 @@ class ClockFacePainter extends CustomPainter {
   
   final hourTextStyle = TextStyle(fontFamily: 'Poppins', fontWeight: FontWeight.w200, fontSize:13.0);
   final minuteTextStyle = TextStyle(fontFamily: 'Poppins', fontWeight: FontWeight.w200, fontSize:10.0);
-  final largeTextStyle = TextStyle(color: Color(0xFF333333), fontFamily: 'Poppins', fontWeight: FontWeight.w400, fontSize:100.0);
+  final largeTextStyle = TextStyle(color: Color(0xFF333333), fontFamily: 'Poppins', fontWeight: FontWeight.w400, fontSize:115.0);
 
   int _hourText({int i, int digitOffset}) { // text offset by digitOffset on the clock
     final hour = (dateTime.hour > 12) ? (dateTime.hour - 12) : dateTime.hour;
